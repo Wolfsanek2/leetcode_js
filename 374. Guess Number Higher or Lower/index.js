a78c07ade40cbdd92337714a88d67e56cd49da7d
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2026-02-04 11:53:20 +0700

    task 374 done

---
+++ 374. Guess Number Higher or Lower/index.js
@@ -0,0 +1,59 @@
+// We are playing the Guess Game. The game is as follows:
+
+// I pick a number from 1 to n. You have to guess which number I picked (the number I picked stays the same throughout the game).
+
+// Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+// You call a pre-defined API int guess(int num), which returns three possible results:
+
+// -1: Your guess is higher than the number I picked (i.e. num > pick).
+// 1: Your guess is lower than the number I picked (i.e. num < pick).
+// 0: your guess is equal to the number I picked (i.e. num == pick).
+// Return the number that I picked.
+
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+let guess;
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+	let left = 1;
+	let right = n;
+	let current = Math.floor((left + right) / 2);
+	let currentGuess = guess(current);
+	while (currentGuess) {
+		if (currentGuess > 0) {
+			left = current + 1;
+		} else {
+			right = current - 1;
+		}
+		current = Math.floor((left + right) / 2);
+		currentGuess = guess(current);
+	}
+	return current;
+};
+
+var testGuessNumber = function (n, pick) {
+	guess = (num) => {
+		if (num < pick) {
+			return 1;
+		}
+		if (num > pick) {
+			return -1;
+		}
+		return 0;
+	};
+	return guessNumber(n);
+};
+
+console.log(testGuessNumber(2, 2));
